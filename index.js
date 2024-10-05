b0efcf8cb4b9e1f362889d76dc0ece0727f8af37
Abdul Aziz <azizskwwe@gmail.com>
2024-10-05 23:38:55 +0530

    new

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const port = 5000;
 const mongoDB = require("./db");
 mongoDB();
 app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     )
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next()
 })
 app.use(express.json())
